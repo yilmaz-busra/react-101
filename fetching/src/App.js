006233f2ae32d749893c6b6619568b28ba29262d
busra <b.busraayilmaz@gmail.com>
2023-05-16 21:34:42 +0300

    fetch added

---
+++ fetching/src/App.js
@@ -0,0 +1,12 @@
+import Users from "./Components/Users";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Users />
+    </div>
+  );
+}
+
+export default App;
---
+++ fetching/src/Components/Users.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+function Users() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+        setIsLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>Users</h1>
+      {isLoading && <div>Loading...</div>}
+      {users.map((user) => {
+        return <div key={user.id}>{user.name}</div>;
+      })}
+    </div>
+  );
+}
+
+export default Users;
