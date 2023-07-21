e3ccd3b7e35ea98c2dedf4e88f56b2dc9151cd76
busra <b.busraayilmaz@gmail.com>
2023-07-22 00:06:49 +0300

    multicontext

--- contxt/src/Components/Profile.js
+++ contxt/src/Components/Profile.js
@@ -1,19 +1,34 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import UserContext from "../contex/UserContext";
 
 function Profile() {
   const { user, setUser } = useContext(UserContext);
-  console.log(user);
+
+  const [loading, setLoading] = useState(false);
 
   const handleLogin = () => {
+    setLoading(true);
     setTimeout(() => {
       setUser({ id: 1, username: "b.busra", bio: "lorem ipseum" });
+      setLoading(false);
     }, 1500);
+  };
+
+  const handleLogout = () => {
+    setUser(null);
   };
   return (
     <div>
-      <button onClick={handleLogin}>Login</button>
+      {!user && (
+        <button onClick={handleLogin}>
+          {loading ? "loading..." : "Login"}
+        </button>
+      )}
+
+      <br />
       <code>{JSON.stringify(user)}</code>
+      <br />
+      {user && <button onClick={handleLogout}>Logout</button>}
     </div>
   );
 }
