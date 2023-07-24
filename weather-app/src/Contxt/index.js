fa81287fed7854ec939c8b237c1988c12b4e2968
busra <b.busraayilmaz@gmail.com>
2023-07-24 22:52:35 +0300

    update

--- weather-app/src/Contxt/index.js
+++ weather-app/src/Contxt/index.js
@@ -1,14 +1,37 @@
-import { useContext, CreateContext, useState } from "react";
-
-const WeatherContext = CreateContext();
+import { useContext, createContext, useState, useEffect } from "react";
+import axios from "axios";
+import cities from "../data/cities.json";
+const WeatherContext = createContext();
 export const WeatherProvider = ({ children }) => {
-  const [weatherData, setWeatherData] = useState(null);
+  const [weatherData, setWeatherData] = useState();
+  const [city, setCity] = useState("İstanbul");
+  const key = " 5feb1ccaefbf73a0af111024f9413deb";
+
+  useEffect(() => {
+    const chosen = cities.filter((city) => city.name === city)[0];
+    const getData = async () => {
+      const { data } = await axios(
+        `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${chosen.name}&days=7&aqi=no&alerts=no`
+      );
+
+      setWeatherData(data.forecast.forecastday);
+    };
+    getData();
+    console.log(weatherData);
+  }, [city]);
+
+  const values = {
+    cities,
+    setCity,
+    city,
+    weatherData,
+  };
 
   return (
-    <WeatherContext.Provider value={{ weatherData, setWeatherData }}>
-      {children}
-    </WeatherContext.Provider>
+    <WeatherContext.Provider value={values}>{children}</WeatherContext.Provider>
   );
 };
 
-export default WeatherContext;
+export const useWeather = () => {
+  return useContext(WeatherContext);
+};
