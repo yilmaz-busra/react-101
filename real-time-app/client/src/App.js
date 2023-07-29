91c7ec091208cee0ede51b2b6c5c6f46f7b8ac65
busra <b.busraayilmaz@gmail.com>
2023-07-29 23:57:36 +0300

    preparingInterface

---
+++ real-time-app/client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Palatte from "./Components/Palatte";
+
+function App() {
+  return (
+    <div className="App">
+      <Palatte />
+    </div>
+  );
+}
+
+export default App;
