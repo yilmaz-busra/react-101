3b36ff833cafcdf20dc6d7cac30093e72407b230
busra <b.busraayilmaz@gmail.com>
2023-07-31 22:49:42 +0300

    socket-backendverigonderme

--- real-time-app/client/src/App.js
+++ real-time-app/client/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 import Palatte from "./Components/Palatte";
 
+import { init } from "./socketApi";
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    init();
+  }, []);
   return (
     <div className="App">
       <Palatte />
---
+++ real-time-app/client/src/socketApi.js
@@ -0,0 +1,17 @@
+import io from "socket.io-client";
+
+let socket;
+//baglantı saglamak icin fonksiyon olusturuldu, bunu app.js comp. uzerinde baglantı olusacak
+export const init = () => {
+  console.log("Sunucuya bağlanılıyor... ");
+  socket = io("http://localhost:3001", {
+    transports: ["websocket"],
+  });
+  socket.on("connect", () => console.log("Sunucuya bağlantı gerçekleşti"));
+};
+// bu fonksiyon bi rengi parametre olarak alıp sunucuya iletecek
+export const send = (color) => {
+  socket.emit("newColor", color);
+  //emit  methodu eger clientta isek backend' e,
+  // backend de isek client a bir data göndermeyi saglar
+};
