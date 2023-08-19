53c42d10520ced3b268942bff375840c965d81e7
busra <b.busraayilmaz@gmail.com>
2023-08-19 21:57:06 +0300

    testing-library

---
+++ testing/src/Components/Counter.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import Counter from "./index";
+
+test("increase btn", () => {
+  render(<Counter />);
+
+  const count = screen.getByText("0");
+  const increaseBtn = screen.getByText("Increase");
+
+  userEvent.click(increaseBtn);
+
+  expect(count).toHaveTextContent("1");
+});
