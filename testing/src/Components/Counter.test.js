3f6f95d3fae6e2283c01ca8450a25260bacf61ce
busra <b.busraayilmaz@gmail.com>
2023-08-24 22:52:37 +0300

    testing-library1

--- testing/src/Components/Counter.test.js
+++ testing/src/Components/Counter.test.js
@@ -1,26 +1,38 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import "@testing-library/jest-dom/extend-expect";
+
 import Counter from "./index";
 
-test("increase btn", async () => {
-  render(<Counter />);
+describe("Counter Tests", () => {
+  let increaseBtn, decreaseBtn, count;
 
-  const count = screen.getByText("0");
-  const increaseBtn = screen.getByText("Increase");
-  userEvent.click(increaseBtn);
-  await(() => 
+  beforeEach(() => {
+    console.log("Her testten önce çalışacağım!");
+    render(<Counter />);
+    increaseBtn = screen.getByText("Increase");
+    decreaseBtn = screen.getByText("Decrease");
+    count = screen.getByText("0");
+  });
+
+  beforeAll(() => {
+    console.log("İlk başta bir kere çalışacağım!");
+  });
+
+  afterEach(() => {
+    console.log("Her testten sonra çalışacağım!");
+  });
+
+  afterAll(() => {
+    console.log("En son bir kere çalışacağım!");
+  });
+
+  test("increase btn", () => {
+    userEvent.click(increaseBtn);
     expect(count).toHaveTextContent("1");
-  );
-});
+  });
 
-test("decrease btn", () => {
-  render(<Counter />);
-
-  const count = screen.getByText("0");
-  const decreaseBtn = screen.getByText("Decrease");
-
-  userEvent.click(decreaseBtn);
-
-  expect(count).toHaveTextContent("-1");
+  test("decrease btn", () => {
+    userEvent.click(decreaseBtn);
+    expect(count).toHaveTextContent("-1");
+  });
 });
